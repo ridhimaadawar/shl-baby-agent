7ed30b9984f631dc734da79aeff61937a8feb63e
Ridhimaa Dawar <dridhimaa@gmail.com>
2026-05-10 17:47:59 +0530

    remove hardcoded api key

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from groq import Groq
 import os
 
 app = FastAPI()
-client = Groq(api_key="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")
+
+client = Groq(api_key=os.environ.get("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"))
 
 @app.get("/health")
 def health():
@@ -13,7 +14,6 @@ def health():
 def chat(request: dict):
     user_message = request.get("message", "")
     
-    # If user says hello, say hello back
     if "hello" in user_message.lower():
         return {
             "reply": "Hello! I help find SHL tests. What job are you hiring for?",
@@ -21,7 +21,6 @@ def chat(request: dict):
             "end_of_conversation": False
         }
     
-    # For everything else, ask a question
     return {
         "reply": "What skills does the job need? Like Java, Python, or leadership?",
         "recommendations": [],
